46e618f9072aff5804788896ae71a93b3195bd19
Hoang Minh Chau <kidphys@gmail.com>
2024-05-15 14:01:14 +0700

    Bump version

--- neurons/miner.py
+++ neurons/miner.py
@@ -168,5 +168,5 @@ class Miner(BaseMinerNeuron):
 if __name__ == "__main__":
     with Miner() as miner:
         while True:
-            bt.logging.info("Miner running...", time.time())
+            bt.logging.info("Miner running v1.0.1...", time.time())
             time.sleep(5)
