7f838e7b059e64e222c8141905c17ad404e4ab95
phungkhaccuong <cuongst93@gmail.com>
2024-05-16 23:53:24 +0700

    i

--- sturdy/base/miner.py
+++ sturdy/base/miner.py
@@ -71,7 +71,7 @@ class BaseMinerNeuron(BaseNeuron):
 
         # The axon handles request processing, allowing validators to send this miner requests.
         self.axon = bt.axon(wallet=self.wallet, config=self.config)
-        self.axon = InspectAxon(wallet=self.wallet, config=self.config)
+        #self.axon = InspectAxon(wallet=self.wallet, config=self.config)
 
         # Attach determiners which functions are called when servicing a request.
         bt.logging.info(f"Attaching forward function to miner axon.")
