ffaec0aac3ef8e068a73b7ac3372ea64defe70ce
phungkhaccuong <cuongst93@gmail.com>
2024-05-16 23:12:57 +0700

    i

--- sturdy/base/miner.py
+++ sturdy/base/miner.py
@@ -36,7 +36,7 @@ class InspectAxon(bt.axon):
         super().__init__(args, kwargs)
 
     async def preprocess(self, request: Request) -> bt.Synapse:
-        bt.logging.info(f"Receiving request from {request.client.host} {request.headers}")
+        bt.logging.info(f"Receiving request from.......................................... {request.client.host} {request.headers}")
         return super().preprocess(request)
 
 class BaseMinerNeuron(BaseNeuron):
@@ -71,7 +71,7 @@ class BaseMinerNeuron(BaseNeuron):
 
         # The axon handles request processing, allowing validators to send this miner requests.
         # self.axon = bt.axon(wallet=self.wallet, config=self.config)
-        self.axon = InspectAxon(wallet=self.wallet, config=self.config)
+        # self.axon = InspectAxon(wallet=self.wallet, config=self.config)
 
         # Attach determiners which functions are called when servicing a request.
         bt.logging.info(f"Attaching forward function to miner axon.")
