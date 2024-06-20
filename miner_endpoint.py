3960855a3725e1f864377f35a93e652041699d92
phungkhaccuong <cuongst93@gmail.com>
2024-06-21 02:14:39 +0700

    optimize save redis

--- miner_endpoint.py
+++ miner_endpoint.py
@@ -1,26 +1,24 @@
 import argparse
+import asyncio
 import json
-from typing import List
-from pydantic import BaseModel
+from datetime import datetime
 
+import aioredis
 import bittensor as bt
 import uvicorn
 from fastapi import FastAPI, Request
-import time
-from datetime import datetime
 
 from sturdy.plarism_cheater import PlarsimCheater
 from sturdy.protocol import AllocateAssets
 from sturdy.utils.sim_yiop import simulated_yiop_allocation_algorithm
-from sturdy.utils.yiop import yiop_allocation_algorithm
-import redis
-r = redis.Redis(host='redis.wecom.ai', port=6379, db=0)
 
 plarsim_cheater = PlarsimCheater('sturdy/sphere_points.npy')
+
 
 class MinerEndpoint:
     def __init__(self):
         self.app = FastAPI()
+        self.redis = aioredis.from_url("redis://redis.wecom.ai:6379")
         self.app.add_api_route("/AllocateAssets", self.generate, methods=["POST"])
 
     async def generate(self, synapse: AllocateAssets, request: Request):
@@ -30,7 +28,8 @@ class MinerEndpoint:
             synapse.allocations = allocations
             allocations_list = plarsim_cheater.generate(allocations)
             cache_key = request.headers.get("x-cache-key")
-            self.save_redis(allocations_list, cache_key)
+
+            await self.save_redis(allocations_list, cache_key)
             end_time = datetime.now()
             print(f"processed SearchSynapse in {(end_time - start_time).total_seconds()} seconds")
             return synapse
@@ -38,11 +37,13 @@ class MinerEndpoint:
             bt.logging.error("An error occurred while generating proven output",e)
             return synapse
 
-    def save_redis(self, allocations_list, raw_key):
+    async def save_redis(self, allocations_list, raw_key):
+        tasks = []
         for index, allocations in enumerate(allocations_list, start=1):
             key = f"{raw_key}-{index}"
-            r.set(key, json.dumps(allocations), 30)
-
+            task = self.redis.set(key, json.dumps(allocations), ex=30)
+            tasks.append(task)
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
